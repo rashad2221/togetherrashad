6bc8d5e3175751be2b472b87e63d9c296d8ed1ff
Tal <talolacat1@gmail.com>
2022-07-21 11:38:55 +0300

    bla bla bla

--- app/src/main/java/com/example/myapplication/Calendar.java
+++ app/src/main/java/com/example/myapplication/Calendar.java
@@ -1,17 +1,31 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.CalendarView;
+import android.widget.TextView;
 
 public class Calendar extends AppCompatActivity {
 
     CalendarView calendarView;
     TextView myDate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        myDate = (TextView) findViewById(R.id.DateText);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView calendarView, int i, int i1, int i2) {
+                String date = (i1 + 1)+ "/" + i2 +"/" + i;
+                myDate.setText(date);
+            }
+        });
     }
 }
