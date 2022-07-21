d4900585e21d3328e455b6e1b409b2d6806a8fa6
Tal <talolacat1@gmail.com>
2022-07-21 11:26:14 +0300

    bla bla bla

--- app/src/main/java/com/example/myapplication/Calendar.java
+++ app/src/main/java/com/example/myapplication/Calendar.java
@@ -3,9 +3,12 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.CalendarView;
 
 public class Calendar extends AppCompatActivity {
 
+    CalendarView calendarView;
+    TextView myDate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
