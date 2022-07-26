b47e20fbcf12f56b6c774a9227ebcd9644df5db9
YanaiZehavi <93211109+YanaiZehavi@users.noreply.github.com>
2022-07-26 17:11:50 +0300

    broken ;(

--- app/src/main/java/com/example/myapplication/SignUpActivity2.java
+++ app/src/main/java/com/example/myapplication/SignUpActivity2.java
@@ -1,25 +1,25 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity2 extends AppCompatActivity  {
     EditText location, idNum, birthday;
     View signup;
     FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
     FirebaseDatabase database;
 
     @Override
@@ -28,7 +28,8 @@ public class SignUpActivity2 extends AppCompatActivity  {
         setContentView(R.layout.activity_sign_up2);
 
         mAuth = FirebaseAuth.getInstance();
-        database = FirebaseDatabase.getInstance("https://together-63e15-default-rtdb.europe-west1.firebasedatabase.app/");
+        database = FirebaseDatabase.getInstance("https://togethermvp-57663-default-rtdb.firebaseio.com/");
+        mDatabase = database.getReference("Users");
 
         signup = findViewById(R.id.signup1);
         location = findViewById(R.id.location);
@@ -48,58 +49,27 @@ public class SignUpActivity2 extends AppCompatActivity  {
                         location.getText().toString(),
                         idNum.getText().toString(),
                         birthday.getText().toString());
-                Intent intent = new Intent(SignUpActivity2.this, SignInActivity.class);
-                startActivity(intent);
             }
         });
     }
 
     public void CreateAccount(String email, String password, String name, String phone, String location, String ID, String birthday){
         mAuth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                                User user = new User(name, email, password, location, ID, phone, birthday);
-                                String uid = mAuth.getCurrentUser().getUid();
-                                database.getReference("Users").child(uid).setValue(user);
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Toast.makeText(SignUpActivity2.this, task.getException().getMessage(),
-                                    Toast.LENGTH_SHORT).show();
-                        }
+                .addOnCompleteListener(this, task -> {
+                    if (task.isSuccessful()) {
+                        // Sign in success, update UI with the signed-in user's information
+
+                        User user = new User(name, email,password, location, ID, phone, birthday);
+                        String uid = mAuth.getCurrentUser().getUid();
+                        mDatabase.child(uid).setValue(user);
+                        Intent intent = new Intent(SignUpActivity2.this, SignInActivity.class);
+                        startActivity(intent);
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(SignUpActivity2.this, task.getException().getMessage(),
+                                Toast.LENGTH_LONG).show();
                     }
                 });
     }
 
-    public void createAccount(String email, String password,
-                              String phoneNum, String location,
-                              String idNum,String UserName, String birthday) {
-        if (email != null && password != null
-                && phoneNum != null && location != null
-                && idNum != null && birthday != null && UserName != null) {
-
-            mAuth.createUserWithEmailAndPassword(email, password)
-                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                        @Override
-                        public void onComplete(@NonNull Task<AuthResult> task) {
-                            if (task.isSuccessful()) {
-                                //    public User(String name, String email, String password, String location, String idNum, String phoneNum, String birthday)
-                                User user = new User (UserName, email,
-                                        password,location, idNum,phoneNum,birthday );
-                                String uid = mAuth.getCurrentUser().getUid();
-                                database.getReference("Users").child(uid).setValue(user);
-                                // Sign in success, update UI with the signed-in user's information
-
-                            } else {
-                                // If sign in fails, display a message to the user.
-                                Toast.makeText(SignUpActivity2.this, task.getException().getMessage(),
-                                        Toast.LENGTH_LONG).show();
-                            }
-                        }
-                    });
-
-        }
-    }
 }
--- app/src/main/java/com/example/myapplication/chooseUser.java
+++ app/src/main/java/com/example/myapplication/chooseUser.java
@@ -1,14 +1,36 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 
 public class chooseUser extends AppCompatActivity {
+    View student, worker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_user);
+        student.setOnClickListener((View.OnClickListener) this);
+        worker.setOnClickListener((View.OnClickListener) this);
+    }
+
+    public void onClick(View view) {
+        if(view == student) {
+            Intent intent = new Intent(chooseUser.this, SignUpActivity.class);
+            startActivity(intent);
+        }
+        else if(view == worker) {
+            Intent intent = new Intent(chooseUser.this, ElderySignUp.class);
+            startActivity(intent);
+        }
     }
 }
