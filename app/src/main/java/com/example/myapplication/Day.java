4e382b2931f576037ff9e1dadf8ac36dd0d3fc71
YanaiZehavi <93211109+YanaiZehavi@users.noreply.github.com>
2022-07-26 09:54:13 +0300

    Update

--- app/src/main/java/com/example/myapplication/Day.java
+++ app/src/main/java/com/example/myapplication/Day.java
@@ -1,19 +1,58 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
+import java.util.List;
 
+import com.google.firebase.database.IgnoreExtraProperties;
+import com.google.firebase.database.ValueEventListener;
+
+@IgnoreExtraProperties
 public class Day {
-    private ArrayList<Event> events;
+    private FirebaseDatabase database;
+    private DatabaseReference mReferenceDays;
+    private List<Event> events = new ArrayList<>();
 
-    public Day(ArrayList<Event> events) {
-        this.events = events;
+    public interface DataStatus {
+        void DataIsLoaded(List<Event> events, List<String> keys);
+        void DataIsInserted();
+        void DataIsUpdated();
+        void DataIsDeleted();
     }
 
     public Day() {
-
+        database = FirebaseDatabase.getInstance();
+        mReferenceDays = database.getReference("Events");
     }
 
-    public ArrayList<Event> getEvents() {
+    public void readEvents(final DataStatus dataStatus) {
+        mReferenceDays.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                events.clear();
+                List<String> keys = new ArrayList<>();
+                for(DataSnapshot keyNode : snapshot.getChildren()) {
+                    keys.add(keyNode.getKey());
+                    Event event = keyNode.getValue(Event.class);
+                    events.add(event);
+                }
+                dataStatus.DataIsLoaded(events, keys);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public List<Event> getEvents() {
         return events;
     }
 
