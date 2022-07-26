f9e6e915dd463f933a267e8b4b5f62fe1c23d294
IdanMar <89003785+IdanMar@users.noreply.github.com>
2022-07-26 09:56:21 +0300

    New Retierment SignUp

---
+++ app/src/main/java/com/example/myapplication/ElderySignUp.java
@@ -0,0 +1,81 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class ElderySignUp extends AppCompatActivity implements View.OnClickListener {
+    EditText username, password, email, location, phoneNum;
+    View signup;
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        email = findViewById(R.id.email);
+        location = findViewById(R.id.location);
+        phoneNum = findViewById(R.id.PhoneNum);
+        signup = findViewById(R.id.signup);
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance("https://together-63e15-default-rtdb.europe-west1.firebasedatabase.app/");
+
+
+        signup.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == signup){
+            createAccount(email.getText().toString(), password.getText().toString() );
+        }
+    }
+
+    public void createAccount(String email, String password) {
+        if(email != null && password != null) {
+            Toast.makeText(this, "works", Toast.LENGTH_LONG).show();
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                House house = new House(username.getText().toString(),
+                                        email,
+                                        password,
+                                        location.getText().toString(),
+                                        phoneNum.getText().toString());
+
+                                String uid = mAuth.getCurrentUser().getUid();
+                                database.getReference("Houses").child(uid).setValue(house);
+                                Intent intent = new Intent(ElderySignUp.this, MainActivity.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                // If sign in fails, display a message to the user.
+
+                                Toast.makeText(ElderySignUp.this, task.getException().getMessage(),
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+}
