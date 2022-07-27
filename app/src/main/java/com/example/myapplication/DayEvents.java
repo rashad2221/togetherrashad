a155713a029eceec9e7ef78f5e53e5569677281a
IdanMar <89003785+IdanMar@users.noreply.github.com>
2022-07-28 00:33:22 +0300

    Sorted by location

--- app/src/main/java/com/example/myapplication/DayEvents.java
+++ app/src/main/java/com/example/myapplication/DayEvents.java
@@ -1,9 +1,17 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +19,8 @@ import java.util.List;
 public class DayEvents extends AppCompatActivity {
     public String dateString;
     private RecyclerView recyclerView;
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,8 @@ public class DayEvents extends AppCompatActivity {
 
         Bundle bundle = getIntent().getExtras();
         dateString = bundle.getString("date");
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance("https://togethermvp-57663-default-rtdb.firebaseio.com/");
 
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview_events);
         new Day().readEvents(new Day.DataStatus() {
@@ -28,10 +40,26 @@ public class DayEvents extends AppCompatActivity {
             public void DataIsLoaded(List<Event> events, List<String> keys) {
 
                 for (Event date : events) {
-                    if (date.getDate().equals(dateString)){
-                        filtered.add(date);
-                        String beep = "bee";
-                    }
+                    String uid = mAuth.getCurrentUser().getUid();
+                    database.getReference("Users").child(uid).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            User currentUser= snapshot.getValue(User.class);
+                            if(date.getLocation().equals(currentUser.getLocation())) {
+                                if (date.getDate().equals(dateString)) {
+                                    filtered.add(date);
+                                    String beep = "bee";
+                                }
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+
 
                 }
 
